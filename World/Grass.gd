d52c6cf4c005f00ac27046040c5f8eac97ed681a
Tim <tim.r.burr@gmail.com>
2022-10-05 09:25:44 -0600

    finished tutorial 15

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,9 +1,11 @@
-extends Sprite
+extends Node2D
+
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
 func create_grass_effect():
-	var grass_effect = load("res://Effects/GrassEffect.tscn").instance()
+	var grass_effect = GrassEffect.instance()
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
-	get_tree().current_scene.add_child(grass_effect)		
 	
 func _on_Hurtbox_area_entered(area):
 	create_grass_effect()
