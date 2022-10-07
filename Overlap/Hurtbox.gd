7d169cc8bc9adbe3e5f22905d6415940f09adbed
Tim <tim.r.burr@gmail.com>
2022-10-07 07:52:54 -0600

    finished tutorial 23

--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -5,7 +5,7 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 var invincible = false setget set_invincible
 
 onready var timer = $Timer
-
+onready var collision_shape = $CollisionShape2D
 signal invincibility_started
 signal invincibility_ended
 
@@ -29,9 +29,8 @@ func _on_Timer_timeout():
 	self.invincible = false
 
 func _on_Hurtbox_invincibility_started():
-	set_deferred("monitorable", false)
+	collision_shape.set_deferred("disabled", true)
 	
 func _on_Hurtbox_invincibility_ended():
-	# set_deferred("monitorable", true)
-	monitorable = true
+	collision_shape.set_deferred("disabled", false)
 
